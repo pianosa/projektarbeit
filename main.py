60e6c517080e5e309244690efa010d96a633b0e9
pianosa <100361170+pianosa@users.noreply.github.com>
2022-06-02 16:42:56 +0200

    First commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import request
 import daten
 import json
+from json import loads
+
 
 app = Flask("Mountrainer")
 
@@ -16,20 +18,18 @@ def index():
 def formular():
     if request.method == 'POST':
         data = request.form
-        vorname = data["vorname"]
-        nachname = data["nachname"]
+        name = data["name"]
         datum = data["datum"]
         zurueckgelegte_Km = data["zurueckgelegte Km"]
         zurueckgelegte_Hm = data["zurueckgelegte Hm"]
         dauer = data["dauer"]
-        temperatur = data["temperatur"]
         try:
             with open("aktivitaeten.json", "r") as open_file: #r für read = lesen
                 datei_inhalt = json.load(open_file)
         except FileNotFoundError:
             datei_inhalt = []
 
-        my_dict = {"Vorname": vorname, "Nachname": nachname, "Datum": datum, "Zurueckgelegte Km": zurueckgelegte_Km, "Zurueckgelegte Hm": zurueckgelegte_Hm, "Temperatur": temperatur, "Dauer": dauer}
+        my_dict = {"Name": name, "Datum": datum, "Zurueckgelegte Km": zurueckgelegte_Km, "Zurueckgelegte Hm": zurueckgelegte_Hm, "Dauer": dauer}
         datei_inhalt.append(my_dict)
 
         with open("aktivitaeten.json", "w") as open_file:
@@ -39,21 +39,12 @@ def formular():
         return render_template("formular.html")
 
 
-@app.route("/analyse/", methods=['GET', 'POST'])
+@app.route("/analyse/")
 def analyse():
     with open("aktivitaeten.json") as open_file:
         json_as_string = open_file.read()
-        data_list = json.loads(json_as_string)
-
-    summe=0
-    for item in data_list:
-        print(item)
-        print(item['Zurueckgelegte Km'])
-        a = item
-        b = item['Zurueckgelegte Km']
-        c = ['a', 'b', 'c']
-
-    return render_template("analyse.html", output=a, km=b, c=c)
+        daten_inhalt = loads(json_as_string)
+    return render_template("analyse.html", daten_inhalt=daten_inhalt)
 
 
 @app.route("/deinjahr/", methods=['GET', 'POST'])
